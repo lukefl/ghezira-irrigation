fed707174a3e8424991c71c3dca51dca0c64aca1
Luke Fraser-Leach <luke.fraserleach@mail.utoronto.ca>
2024-03-16 12:45:47 -0400

    Write function to load CMIP6 data
    Currently the function loads a single variable historical and
    ssp370. In a future commit it will be rewritten to load a variable
    for a single experiment.

--- luke/plot_climatology.py
+++ luke/plot_climatology.py
@@ -1,5 +1,3 @@
-#%%
-# import modules
 import numpy as np
 import pandas as pd
 import geopandas as gpd
@@ -8,84 +6,84 @@ import xarray as xr
 import rioxarray as rxr
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
-import xesmf as xe
-import xclim.sdba as sdba
+from irrigation_water_demand import *
 
-name = 'precip'
+#### PATHS ###
+path_to_data = '/Users/lfl/utcdw_data'
+shapefile_name = f"{path_to_data}/Gezira.shp"
+df_shapefile = gpd.read_file(shapefile_name, crs="epsg:4326")
 
-#%%
-# open shapefile
-data_dir = f'/Users/lfl/utcdw_data'
-
-file_in = f"{data_dir}/Gezira.shp"
-
-df_shapefile = gpd.read_file(file_in, crs="epsg:4326")
-df_shapefile.plot()
-
-def mask_region(arr,xdim,ydim):
-    arr = arr.rio.set_spatial_dims(x_dim=xdim, y_dim=ydim)
+#### FUNCTIONS ####
+def mask_region(arr,ds='era5'):
+    if ds=='era5':
+        arr = arr.rio.set_spatial_dims(x_dim="longitude", y_dim="latitude")
+    elif ds=='tc':
+        arr = arr.rio.set_spatial_dims(x_dim="lon", y_dim="lat")
     arr = arr.rio.write_crs("epsg:4326")
     return arr.rio.clip(df_shapefile.geometry.values, df_shapefile.crs, drop = False, invert = False)
 
-# start, end = 1980, 2011
-yrs = end - start
-days = 365
-tmin_dat=np.empty((yrs, days))
-rh_dat=np.empty((yrs, days))
-tmax_dat=np.empty((yrs, days))
 
-tmin_dat[:]=np.nan
-rh_dat[:]=np.nan
-tmax_dat[:]=np.nan
-
-#%%
-# test for relative humidity
-rh_file = f"RH.e5.daily.highres.1980-2010.nc"
-tmin_file = f"tmin.e5.daily.highres.1980-2010.nc"
-tmax_file = f"tmax.e5.daily.highres.1980-2010.nc"
-precip_file = f"precip.e5.daily.highres.1980-2010.nc"
-
-f_rh = xr.open_dataset(f"{data_dir}/{rh_file}")
-# f_rh.dims
-rh=mask_region(f_rh.relative_humidity,xdim='lat',ydim='lon')
-# rh[0].plot()
-
-#%%
-# open ERA5 data sample
-# for year in range(start,end):
-f_rh = xr.open_dataset(f"{data_dir}/{rh_file}")
-f_tmin = xr.open_dataset(f"{data_dir}/{tmin_file}")
-f_tmax = xr.open_dataset(f"{data_dir}/{tmax_file}")
-# j = year - start
-tmin=f_tmin.t2m * 1000 # do unit conversion
-tmin.attrs['units'] = 'mm/day' # update the unit attributes
-tmin = mask_region(tmin,xdim='lat',ydim='lon')
-#%%
-rh=mask_region(f_rh.relative_humidity,xdim='lat',ydim='lon')
-tmax=mask_region(f_tmax.t2m,xdim='lat',ydim='lon')
-rh_dclim = rh.groupby('time.dayofyear').mean('time')
-tmax_dclim = rh.groupby('time.dayofyear').mean('time')
-# for i in range(days):  # how to deal with leap years??
-#     tmin_dat[j,i] = np.nanmean(tmin[i,:,:])
-#     rh_dat[j,i] = np.nanmean(rh[i,:,:])
-#         # t2m_dat[j,i] = np.nanmean(t2m[i,:,:])
-
-#%%
-def plot(dat, title, unit):
-    clim = dat.mean(dim=['lat','lon'])
-    max=dat.max
-    min=dat.min
-    time=clim.dayofyear
+def plot(dat, title, unit, label):
     plt.figure(figsize=(10,5))
-    plt.plot(time,clim,label='ERA5')
+    clim = np.nanmean(dat,axis=0)
+    max=np.nanmax(dat,axis=0)
+    min=np.nanmin(dat,axis=0)
+    time=np.arange(days)
+    plt.plot(time,clim,label=label)
     plt.fill_between(time,min,max,alpha=0.5)
+
     plt.grid()
     plt.xlim(0,364)
     plt.legend()
     plt.xlabel('Days since January 1st')
-    # plt.title(f'Mean Regional {title} Climatology {start}-{end-1} ({unit})')
+    plt.title(f'Mean Regional {title} Climatology {start}-{end-1} ({unit})')
 
-# plot(tmin, 'Daily Precipitation', 'mm/day')
-plot(rh_dclim, 'Relative Humidity', '%')
-# plot(tmax, '2m Temperature', 'K')
-# %%
+
+# initialize arrays for terraclim data
+start, end = 1980, 2011
+yrs = end - start
+days = 365
+
+tp_tc=np.empty((yrs, days))
+rh_tc=np.empty((yrs, days))
+t2m_tc=np.empty((yrs, days))
+re_tc=np.empty((yrs, days))
+
+tp_tc[:]=np.nan
+rh_tc[:]=np.nan
+t2m_tc[:]=np.nan
+re_tc[:]=np.nan
+
+print('loading terraclim...')
+f_tp = xr.open_dataset(path_to_data+f"precip.e5.daily.highres.{start}-{end-1}.nc")
+f_rh = xr.open_dataset(path_to_data+f"RH.e5.daily.highres.{start}-{end-1}.nc")
+f_tmax = xr.open_dataset(path_to_data+f"tmax.e5.daily.highres.{start}-{end-1}.nc")
+f_tmin = xr.open_dataset(path_to_data+f"tmin.e5.daily.highres.{start}-{end-1}.nc")
+
+tp=f_tp.tp * 1000 # do unit conversion
+tp.attrs['units'] = 'mm/day' # update the unit attributes
+tp = mask_region(tp,ds='tc')
+rh= mask_region(f_rh.relative_humidity,ds='tc')
+tmax= mask_region(f_tmax.t2m,ds='tc')
+tmin= mask_region(f_tmin.t2m,ds='tc')
+t2m = (tmax + tmin) / 2
+
+
+# Check that shapefile is working 
+#rh[0].plot()
+#plt.show()
+
+for year in range(start, end):
+    j = year - start 
+    for i in range(days):
+        k = i + j * 365
+        tp_tc[j,i] = np.nanmean(tp[k,:,:])
+        rh_tc[j,i] = np.nanmean(rh[k,:,:])
+        t2m_tc[j,i] = np.nanmean(t2m[k,:,:])
+        re_tc[j,i] = reference_crop_evapotranspiration(t2m_tc[j,i], rh_tc[j,i])
+
+plot(tp_tc, 'Daily Precipitation', 'mm/day', 'TerraClim')
+plot(rh_tc, 'Relative Humidity', '%', 'TerraClim')
+plot(t2m_tc, '2m Temperature', 'K', 'TerraClim')
+plot(re_tc, 'Reference Evapotransporation', 'K', 'TerraClim')
+plt.show()
