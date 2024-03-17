e347bbccad97c4a2f7704118ef16d55de400c9a7
Luke Fraser-Leach <luke.fraserleach@mail.utoronto.ca>
2024-03-16 20:49:28 -0400

    Continue to build dbcca script

--- luke/dbcca.py
+++ luke/dbcca.py
@@ -15,6 +15,15 @@ import xesmf as xe
 #%%
 # set paths
 path_to_data = '/Users/lfl/utcdw_data/'
+data_transfer = ('/Users/lfl/google_drive/phd/utcdw_hackathon/' +
+    'ghezira-irrigation/data-transfer')
+
+tas_his_fname = 'tas.cesm.daily.historical.1980-2010.nc'
+tas_370_fname = 'tas.cesm.daily.ssp370.2070-2100.nc'
+pr_his_fname = 'precip.cesm.daily.historical.1980-2010.nc'
+pr_370_fname = 'precip.cesm.daily.ssp370.2070-2100.nc'
+hur_his_fname = 'RH.cesm.daily.historical.1980-2010.nc'
+hur_370_fname = 'RH.cesm.daily.ssp370.2070-2100.nc'
 
 # set output file names
 tas_his_bcca_file = f'{path_to_data}tas_his_bcca.nc'
@@ -34,17 +43,23 @@ hur_370_dbcca_file = f'{path_to_data}hur_370_dbcca.nc'
 
 #%%
 # load CESM data
-start = 1980
-end = 2011
-years_his = np.arange(1980,2011, 1)
-years_585 = np.arange(2070, 2011, 1)
-member_id = 'r10i1p1f1'
-tas_his = gl.load_var(
-    var='tas',experiment_id='historical', member_id=member_id, years=years_his)
-tas_585 = gl.load_var(
-    var='tas',experiment_id='ssp585', member_id=member_id, years=years_585)
-# pr_his = gl.load_var(
-#     var='tas',experiment_id='historical', years=years_his)
+# start = 1980
+# end = 2011
+# years_his = np.arange(1980,2011, 1)
+# years_585 = np.arange(2070, 2011, 1)
+# member_id = 'r10i1p1f1'
+tas_his = xr.load_dataset(f'{data_transfer}/{tas_his_fname}')
+tas_370 = xr.load_dataset(f'{data_transfer}/{tas_370_fname}')
+#
+pr_his = xr.load_dataset(f'{data_transfer}/{pr_his_fname}')
+pr_370 = xr.load_dataset(f'{data_transfer}/{pr_370_fname}')
+#
+hur_his = xr.load_dataset(f'{data_transfer}/{hur_his_fname}')
+hur_370 = xr.load_dataset(f'{data_transfer}/{hur_370_fname}')
+# tas_his = gl.load_var(
+#     var='tas',experiment_id='historical', member_id=member_id, years=years_his)
+# tas_585 = gl.load_var(
+#     var='tas',experiment_id='ssp585', member_id=member_id, years=years_585)
 
 #%% load obs
 f_tmax = xr.open_dataset(path_to_data +
